63fbe9b648f0d63222b0819405bbc05939eaa581
invalidcode232 <mail@invalidcode.me>
2025-05-04 18:25:19 +0800

    refactor(types): enhance type definitions for events and external data

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,14 +2,22 @@ type CalendarEvent = {
     summary: string;
     startTime?: string | null;
     endTime?: string | null;
+    // we would also add location and others
 };
+
+type ExternalData = { // storage to put weather, traffic time, etc.
+    name: string;
+    value: string;
+}
 
 type NewEvent = {
     summary: string;
     startTime?: string | null;
     endTime?: string | null;
     location?: string | null;
+    externalData?: ExternalData[];
 };
+
 
 interface AISummarizerPayload {
     currentCondition: string;
@@ -18,8 +26,8 @@ interface AISummarizerPayload {
 }
 
 interface AISchedulerPayload {
-    currentCondition: string;
-    currentTemperature: number;
+    currentCondition?: string;
+    currentTemperature?: number;
     events: CalendarEvent[];
     newEvent?: NewEvent;
 }
