a7ea5fc56e3a6e135ce9c5f95729eb75f9e4d4b8
invalidcode232 <invalidcode232@protonmail.com>
2025-05-05 16:23:20 +0800

    feat(google)!: update scope for calendar adding

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,7 +7,7 @@ import { google } from 'googleapis';
 import type { JSONClient } from 'google-auth-library/build/src/auth/googleauth';
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
